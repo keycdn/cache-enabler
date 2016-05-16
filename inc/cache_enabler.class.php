d4ed2751f36c03e0081756e59f69a8e1f5506926
keycdn <keycdn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-05-16 16:39:18 +0000

    added permission check
    git-svn-id: http://plugins.svn.wordpress.org/cache-enabler/trunk@1418025 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/cache_enabler.class.php
+++ inc/cache_enabler.class.php
@@ -1551,6 +1551,23 @@ final class Cache_Enabler {
 			);
 		}
 
+		// permission check
+		if ( !is_writable( CE_CACHE_DIR ) ) {
+			show_message(
+				sprintf(
+					'<div class="error"><p>%s</p></div>',
+					sprintf(
+						__('The <b>%s</b> requires write permissions %s on %s. Please <a href="%s" target="_blank">change the permissions</a>.', 'cache-enabler'),
+						'Cache Enabler',
+						'755',
+						'wp-content/cache/cache-enabler',
+						'http://codex.wordpress.org/Changing_File_Permissions',
+						CE_MIN_WP
+					)
+				)
+			);
+		}
+
 		// mb string check
 		if ( !function_exists('mb_convert_encoding') && $options['webp'] ) {
 			show_message(
