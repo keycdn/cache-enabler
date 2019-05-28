1b1275d381adfdaf1b0555eefc9fd78765f1b59c
auban <auban@stag-agency.be>
2019-05-29 00:59:38 +0200

    Add user agent

--- cache-enabler.php
+++ cache-enabler.php
@@ -6,7 +6,7 @@ Description: Simple and fast WordPress disk caching plugin.
 Author: ViaMonkey
 Author URI: https://www.viamonkey.com
 License: GPLv2 or later
-Version: 2.0.2
+Version: 2.0.3
 */
 
 /*
